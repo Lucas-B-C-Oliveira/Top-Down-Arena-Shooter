a4e9c2f4f4ac1c90d3279595eabf68ffa5a90cf5
Lucas B. Cesar <33939027+lcsbc123@users.noreply.github.com>
2019-11-26 20:13:34 -0300

    fix player speed and player system die

--- Game/Game_Utils/Scripts/Enemy1.gd
+++ Game/Game_Utils/Scripts/Enemy1.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var target_player
-var speed = 25
+var speed = 15
 var dir = Vector2(0 , -1)
 var my_direction
 
@@ -29,9 +29,10 @@ func _process(delta):
 		return
 	
 	if life_to_stop:
+		life_to_stop = false
 		number_of_dies += 1
 	
-	if number_of_dies == 3:
+	if number_of_dies >= 3:
 		GAME_MANAGER.followers_enemys_die += 1
 		GAME_MANAGER.stop_me(id)
 		return
@@ -119,7 +120,8 @@ func _on_area_body_entered(body):
 		
 		##### BUFFS
 		
-		speed += 5 * number_of_dies
+		speed += 1.1 * number_of_dies
+		print("Encostou no player")
 	pass
 
 
@@ -137,11 +139,7 @@ func _on_area_area_entered(bullet):
 		
 		##### BUFFS
 		
-		speed += 10 * number_of_dies
+		speed += 2 * number_of_dies
+		print("number_of_dies" + str(number_of_dies))
 
 
-func _on_area_body_shape_entered(body_id, body, body_shape, area_shape):
-#	if body.has_method("take_damage"):
-#		print("Colidiu com o player")
-#		body.take_damage(1)
-	pass # Replace with function body.
